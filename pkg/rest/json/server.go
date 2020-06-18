c1304c88f88d77d923dbe2a186308251ee2829a2
mark <mark.poling@gmail.com>
2020-06-18 00:14:49 -0400

    Second run at moving ServeJSON out of the services space.

--- pkg/rest/json/server.go
+++ pkg/rest/json/server.go
@@ -4,12 +4,32 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+
+	"github.com/gojou/goof/pkg/models"
+	"github.com/gojou/goof/pkg/services/fencer"
+	"github.com/gorilla/mux"
 )
 
-// ServeJSON serves the list of fencers in JSON format
-func (s *service) ServeJSON(w http.ResponseWriter, r *http.Request) {
-	flist, _ := s.r.ListFencers()
-	json, _ := json.MarshalIndent(flist, "", "  ")
-	fmt.Fprintln(w, string(json))
+// Server defines the methods on the server
+type Server interface {
+	ServeJSON(*[]models.Fencer, error) http.HandlerFunc
+}
 
+type server struct {
+	s *fencer.Service
+	m *mux.Router
+}
+
+// NewServer instantiates the server
+func NewServer(s *fencer.Service, m *mux.Router) Server {
+	return &server{s: s, m: m}
+}
+
+// ServeJSON serves the list of fencers in JSON format
+func (s *server) ServeJSON(fencers *[]models.Fencer, err error) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+
+		json, _ := json.MarshalIndent(fencers, "", "  ")
+		fmt.Fprintln(w, string(json))
+	}
 }
